c1515f3179f8f40eecf55436e046f78237a1118e
ali <alidaher9197@gmail.com>
2025-08-30 17:19:22 +0300

    we add image to the title

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en"><!--english language will be used in this page-->
+<head>    
+    <meta charset="UTF-8"><!--this mean i am using most used letters -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Work 2</title>
+    <link rel="icon" type="html_icon.png" href="assets/emoji.png" class="title-logo">
+    
+</head>
+<body>
+    
+</body>
+</html>
