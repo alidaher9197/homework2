475adeb62edc91a58ae36b03dfc7f7b87ab88641
ali <alidaher9197@gmail.com>
2025-08-30 19:38:17 +0300

    using lists

--- index2.php
+++ index2.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lists</title>
 </head>
 <body>
-    
+    stages
+    <ol start="5">
+        <li>get objects</li>
+        <li>organize them</li>
+        <li>use ai</li>
+    </ol>
+        
+    items
+    <ul>
+        <li>template</li>
+        <li>pc</li>
+        <li>skills</li>
+    </ul>
+
+    <dl>
+<dt>HTML</dt>
+<dd>hypertext markup language</dd>
+    </dl>
 </body>
 </html>
