75bf57e86ecf83b468547911597200b95595b754
ali <alidaher9197@gmail.com>
2025-08-30 19:16:32 +0300

    write examples of html tags

--- index.php
+++ index.php
@@ -4,10 +4,53 @@
     <meta charset="UTF-8"><!--this mean i am using most used letters -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Work 2</title>
-    <link rel="icon" type="html_icon.png" href="assets/emoji.png" class="title-logo">
+    
+    <link rel="icon" type="html_icon.png" href="assets/emoji.png" class="title-logo"><!--this image is in the title-->
     
 </head>
 <body>
-    
+    <h1 style="display: inline-block;">This is header 1</h1>
+    <h1 style="display: inline-block;">This is header 1</h1>
+    <h2>This is header 2</h2>
+    <h3>This is header 3</h3>
+    <h4>This is header 4</h4>
+    <h5>This is header 5</h5>
+    <h6>This is header 6</h6>
+    <hr><!--horizontal line-->
+    <p>
+        This is a paragraph
+    </p><!--this is paragraph-->
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi libero fugiat distinctio consequatur ipsum
+        <br> natus tempora doloremque culpa voluptates corrupti saepe veniam dolorem ab repudiandae facilis, delectus, consectetur dignissimos nostrum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae unde quo eaque enim quod voluptatibus officia quos minima esse libero dolor, repellat ratione ut, temporibus vel porro nisi deserunt? Nam.</p>
+    <hr>
+    <br><!-- new line-->
+    <p>
+        <em>this is itallic</em>
+        <br>
+        <i>this is itallic</i>
+        <br>
+        <b>this is bolded</b>
+        <br>
+        <strong>this is bolded</strong>
+        <br>
+        <mark>this is marked</mark>
+        <br>
+        <u>this is underlined</u>
+        <br>
+        O<sub>2</sub>
+        <br>
+        2<sup>3</sup>
+        
+        <pre>
+            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, corporis? Quia aspernatur sunt, expedita incidunt minus neque esse aut aperiam sequi nihil ut provident nesciunt libero porro sapiente reiciendis dignissimos?
+        </pre>
+       
+    </p>
+    <address>
+        Lebanon <br>
+        Beirut <br>
+        Shweifat <br>
+    </address>
 </body>
 </html>
